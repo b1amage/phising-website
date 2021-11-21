810a30b4e00ff39d4bca060ccba534c7d7b6b80e
s3877698 <s3877698@rmit.edu.vn>
2021-11-21 13:03:19 +0700

    update facebook clone

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const path = require('path');
-const pathPublic = path.join(__dirname, './public');
+const path = require("path");
+const pathPublic = path.join(__dirname, "./public");
 app.use(express.static(pathPublic));
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 const port = 4000;
 
-app.get('/', (req, res) => {
-    // res.send('Hello');
-    let params = req.query;
+app.get("/", (req, res) => {
+  // res.send('Hello');
+  let params = req.query;
 
-    let email = params.user_email;
-    let password = params.password;
+  let email = params.email;
+  let password = params.password;
 
-    console.log(email, password);
-    res.render('index');
-})
-
+  console.log(email, password);
+  res.render("facebook");
+});
 
 app.listen(port, () => {
-    console.log(`App started at ${port}`);
+  console.log(`App started at ${port}`);
 });
